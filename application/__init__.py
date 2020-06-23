9b249d6bc17b96496865e7e045b6070c32cfc4d8
martialo12 <martialo218@gmail.com>
2020-06-23 07:56:59 +0200

    bugFixed

--- application/__init__.py
+++ application/__init__.py
@@ -18,7 +18,7 @@ bcrypt = Bcrypt()
 
 def create_app(config_type):  # development, production, testing
     app = Flask(__name__)
-    config_file = os.path.join(os.getcwd() + 'config', config_type + '.py')
+    config_file = os.path.join(os.getcwd() + '/config', config_type + '.py')
     app.config.from_pyfile(config_file)
     # initialise application wwith db instance
     db.init_app(app)
